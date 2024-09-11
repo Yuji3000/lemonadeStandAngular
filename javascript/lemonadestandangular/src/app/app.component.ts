16e1019d9c5b7015c5ea6919ff6bc7464fe64900
Yuji Kosakowski <yuji.k.dev@gmail.com>
2024-09-10 21:56:20 -0600

    Initial commit and start practice project

---
+++ javascript/lemonadestandangular/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { LemonadeComponent } from "./lemonade/lemonade.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, LemonadeComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'lemonadestandangular';
+}
